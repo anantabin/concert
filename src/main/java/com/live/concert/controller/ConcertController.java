854a74a7317e808f69c8a579891fcde5f877cff7
ananta <anantabin@gmail.com>
2024-04-07 10:35:07 +0700

    [FEAT] Get Concert Detail API

--- src/main/java/com/live/concert/controller/ConcertController.java
+++ src/main/java/com/live/concert/controller/ConcertController.java
@@ -2,6 +2,7 @@ package com.live.concert.controller;
 
 import com.live.concert.contract.GetConcertResponse;
 import com.live.concert.entity.Concert;
+import com.live.concert.exception.ConcertNotFoundException;
 import com.live.concert.service.ConcertService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -9,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.time.LocalDate;
 
 @RestController
-@RequestMapping("/concerts")
+@RequestMapping("/concert")
 public class ConcertController {
     @Autowired
     private ConcertService concertService;
@@ -32,6 +34,20 @@ public class ConcertController {
         Pageable pageable = PageRequest.of(page, size);
         Page<GetConcertResponse> concerts = concertService.filterConcerts(name, concertStartDate, concertEndDate, pageable).map(this::mapToGetConcertResponse);
         return ResponseEntity.ok(concerts);
+    }
+
+    @GetMapping("/{concertId}")
+    public ResponseEntity<GetConcertResponse> getConcerts(
+            @PathVariable long concertId
+    ) {
+        try {
+            return ResponseEntity.ok(mapToGetConcertResponse(concertService.getConcertById(concertId)));
+        } catch (ConcertNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+
     }
 
     private GetConcertResponse mapToGetConcertResponse(Concert concert) {
